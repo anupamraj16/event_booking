747547bae16897878185ce8675b4e4e010547d9b
anupamraj16 <72901179+anupamraj16@users.noreply.github.com>
2024-03-28 12:09:47 -0600

    authentication and route protection added

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,19 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/anupamraj16/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+	authenticatedRoutes := server.Group("/")
+	authenticatedRoutes.Use(middlewares.Authenticate)
+	// server.POST("/events", middlewares.Authenticate, createEvent)
+	authenticatedRoutes.POST("/events", createEvent)
+	authenticatedRoutes.PUT("/events/:id", updateEvent)
+	authenticatedRoutes.DELETE("/events/:id", deleteEvent)
 	server.POST("/signup", createUser)
 	server.POST("/login", login)
 }
